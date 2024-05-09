1cf3619c6782486c42ec201c651cc3c6b3c975da
Agnès RAMIANDRISOA <agnesrami@yahoo.fr>
2024-05-09 12:17:53 +0200

    tested client, compte creation in main

--- src/main/java/com/example/demo/ProxiBanqueAeaApplication.java
+++ src/main/java/com/example/demo/ProxiBanqueAeaApplication.java
@@ -21,16 +21,16 @@ public class ProxiBanqueAeaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxiBanqueAeaApplication.class, args);
 		//Créer des nouveaux clients
+		//Créer des nouveaux comptes des 2 types
 		Adresse a = new Adresse("ville", "473829","012234");
 		LocalDate date = LocalDate.now();
 		CompteCourant cc = new CompteCourant("1234", date, 500, "courant");
-		CompteEpargne ce = new CompteEpargne("12345", date, 500, "epargnes");
+		CompteEpargne ce = new CompteEpargne("12345", date, 500, "epargne");
 		List<Compte> comptes = new ArrayList<Compte>();
 		comptes.add(cc);
 		comptes.add(ce);
 		Client c = new Client("nom","prenom",a,comptes);
 		System.out.println(c);
-		//Créer des nouveaux comptes des 2 types
 		//Tester opérations crédit et débit
 	}
 
