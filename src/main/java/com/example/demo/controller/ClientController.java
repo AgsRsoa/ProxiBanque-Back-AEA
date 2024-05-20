b787b0d8439bf844245b60def90f7d063b66838c
Agnes Ramiandrisoa <agnesrami@yahoo.fr>
2024-05-20 15:31:26 +0200

    added show client to controller

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,11 @@ public class ClientController {
 	@GetMapping
 	List<Client> allClients(){
 		return service.getAllClients();
+	}
+	
+	@GetMapping("{id}")
+	Optional<Client> oneClient(@PathVariable Long id){
+		return service.getClientById(id);
 	}
 	
 	@PostMapping
